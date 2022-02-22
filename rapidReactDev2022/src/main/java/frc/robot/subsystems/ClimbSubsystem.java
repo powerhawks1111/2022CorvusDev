8e7b88e391f7c1a7777e511f564ca390059f2ae6
rex-mcall <rexmcall@gmail.com>
2022-02-21 22:22:28 -0500

    F1P-75 work on climb

--- rapidReactDev2022/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
+++ rapidReactDev2022/src/main/java/frc/robot/subsystems/ClimbSubsystem.java
@@ -1,13 +1,22 @@
 package frc.robot.subsystems;
 
+import com.revrobotics.SparkMaxPIDController;
+import com.revrobotics.CANSparkMax.ControlType;
+
 import frc.robot.variables.Motors;
 
 public class ClimbSubsystem {
+    private double minEncoderVal = 0;
+    private double maxEncoderVal = 600;
+    private SparkMaxPIDController climbPID = Motors.climbLeader.getPIDController();
     /**
      * Constructor
      */
     public ClimbSubsystem() {
-
+        //TODO tune these later
+        climbPID.setP(0.1);
+        climbPID.setI(0.0001);
+        climbPID.setOutputRange(-1, 1);
     }
 
     /**
@@ -15,6 +24,20 @@ public class ClimbSubsystem {
      * @param speed -1 to +1 speed for climb motor
      */
     public void driveClimbMotor(double speed) {
-        Motors.climbLeader.set(speed);
+        climbPID.setReference(speed, ControlType.kDutyCycle);
+    }
+
+    /**
+     * Uses PID to extend the climb to its maximum height
+     */
+    public void extendClimb() {
+        climbPID.setReference(maxEncoderVal, ControlType.kPosition);
+    }
+
+    /**
+     * Uses PID to retract the climb in to its minimum height
+     */
+    public void retractClimb() {
+        climbPID.setReference(minEncoderVal, ControlType.kPosition);
     }
 }
